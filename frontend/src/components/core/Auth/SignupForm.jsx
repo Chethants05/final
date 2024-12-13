53b0116e3d9ea6cc76f34d35d91100c0c9d78eae
Bhuvan-Hegde <bhuvanshegde@gmail.com>
2024-12-13 12:46:15 +0530

    Update SignupForm.jsx

--- frontend/src/components/core/Auth/SignupForm.jsx
+++ frontend/src/components/core/Auth/SignupForm.jsx
@@ -73,16 +73,16 @@ function SignupForm() {
 
   // data to pass to Tab component
   const tabData = [
-    {
-      id: 1,
-      tabName: "Student",
-      type: ACCOUNT_TYPE.STUDENT,
-    },
-    {
-      id: 2,
-      tabName: "Instructor",
-      type: ACCOUNT_TYPE.INSTRUCTOR,
-    },
+    // {
+    //   id: 1,
+    //   // tabName: "Student",
+    //   type: ACCOUNT_TYPE.STUDENT,
+    // },
+    // {
+    //   id: 2,
+    //   // tabName: "Instructor",
+    //   type: ACCOUNT_TYPE.INSTRUCTOR,
+    // },
   ];
 
   return (
