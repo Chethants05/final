ed3612f5efd6e281787e635349ed6810eae4c392
Chethants05 <147964666+Chethants05@users.noreply.github.com>
2024-12-14 13:07:30 +0530

    Update rajorpay.js

--- backend/config/rajorpay.js
+++ backend/config/rajorpay.js
@@ -2,6 +2,6 @@ const Rajorpay = require('razorpay');
 require('dotenv').config();
 
 exports.instance = new Rajorpay({
-    key_id: rzp_live_eMweeorqkvWbCJ,
-    key_secret: r94Eigdonunvat4dakEfQVKe
+    key_id: 'rzp_live_eMweeorqkvWbCJ',
+    key_secret: 'r94Eigdonunvat4dakEfQVKe'
 })
